c21116d6e297981672cf87a0bd010c12ebbded04
lucasmoraist <luksmnt1101@gmail.com>
2026-01-16 10:03:08 -0300

    first commit

---
+++ src/test/java/com/lucasmoraist/notification_svc/NotificationSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasmoraist.notification_svc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
